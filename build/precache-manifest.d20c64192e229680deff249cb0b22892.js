7f08e1c0112af7172e0c4d1fadcdc76c9aa77f9e
ishikabansal04 <ikabansal04@gmail.com>
2021-11-15 10:57:57 +0530

    Adding source code for keeper app

---
+++ build/precache-manifest.d20c64192e229680deff249cb0b22892.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0307088e2b1f098dde2f5f593870e5",
+    "url": "/KeeperApp-React/index.html"
+  },
+  {
+    "revision": "f688d5f2256f64470d59",
+    "url": "/KeeperApp-React/static/js/2.aa89413b.chunk.js"
+  },
+  {
+    "revision": "4b56d93c478f64733ad0",
+    "url": "/KeeperApp-React/static/js/main.1fb37fd3.chunk.js"
+  },
+  {
+    "revision": "7391ede009c6ae0378f7",
+    "url": "/KeeperApp-React/static/js/runtime-main.5073af64.js"
+  }
+]);
